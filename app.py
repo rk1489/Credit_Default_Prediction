cfa24fbfce5210ead50a5a3eb651d503996344f0
Raushan Kumar <69127358+rk1489@users.noreply.github.com>
2020-12-04 02:04:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,33 +24,33 @@ def predict_default(features):
 def main():
 
     html_temp = """
-        <div style = "background-color:blue; padding: 10px;">
+        <div style = "background-color:pink; padding: 10px;">
             <center><h1>Credit Default Predictor ML App</h1></center>
         </div><br>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
 
-    LIMIT_BAL = st.text_input("Limit Balance (in $)")
+    LIMIT_BAL = st.text_input("Limit Balance (in NT $)")
     
     education_status = ["Graduate School", "University", "High School", "Others"]
     marital_status = ["Married","Single", "others"]
 
     payment_status = [
-        "Account started that month with a zero balance, and never used any credit.",
-        "Account had a balance that was paid in full.",
-        "At least the minimum payment was made, but the entire balance wasn't paid.",
-        "Payment delay for 1 month.",
-        "Payment delay for 2 month.",
-        "Payment delay for 3 month.",
-        "Payment delay for 4 month.",
-        "Payment delay for 5 month.",
-        "Payment delay for 6 month.",
-        "Payment delay for 7 month.",
-        "Payment delay for 8 month.",   
+        "Account started that month with a zero balance, and never used any credit",
+        "Account had a balance that was paid in full",
+        "At least the minimum payment was made, but the entire balance wasn't paid",
+        "Payment delay for 1 month",
+        "Payment delay for 2 month",
+        "Payment delay for 3 month",
+        "Payment delay for 4 month",
+        "Payment delay for 5 month",
+        "Payment delay for 6 month",
+        "Payment delay for 7 month",
+        "Payment delay for 8 month",   
     ]
 
     EDUCATION = education_status.index(st.selectbox(
-        "Select Education",
+        "Education",
         tuple(education_status)
     )) + 1
     
@@ -66,19 +66,19 @@ def main():
         tuple(payment_status)
     )) - 2
      
-    BILL_AMT1 = st.text_input("Last month Bill Amount ( in (NT) $ )")
-    BILL_AMT2 = st.text_input("Last 2nd month Bill Amount ( in (NT) $ )")
-    BILL_AMT3 = st.text_input("Last 3rd month Bill Amount ( in (NT) $ )")
-    BILL_AMT4 = st.text_input("Last 4th month Bill Amount ( in (NT) $ )")
-    BILL_AMT5 = st.text_input("Last 5th month Bill Amount ( in (NT) $ )")
-    BILL_AMT6 = st.text_input("Last 6th month Bill Amount ( in (NT) $ )")
+    BILL_AMT1 = st.text_input("Last month Bill Amount ( in NT $ )")
+    BILL_AMT2 = st.text_input("Last 2nd month Bill Amount ( in NT $ )")
+    BILL_AMT3 = st.text_input("Last 3rd month Bill Amount ( in NT $ )")
+    BILL_AMT4 = st.text_input("Last 4th month Bill Amount ( in NT $ )")
+    BILL_AMT5 = st.text_input("Last 5th month Bill Amount ( in NT $ )")
+    BILL_AMT6 = st.text_input("Last 6th month Bill Amount ( in NT $ )")
 
-    PAY_AMT1 = st.text_input("Amount paid in Last Month ( in (NT) $)")
-    PAY_AMT2 = st.text_input("Amount paid in Last 2nd month ( in (NT) $ )")
-    PAY_AMT3 = st.text_input("Amount paid in Last 3rd month ( in (NT) $ )")
-    PAY_AMT4 = st.text_input("Amount paid in Last 4th month ( in (NT) $ )")
-    PAY_AMT5 = st.text_input("Amount paid in Last 5th month ( in (NT) $ )")
-    PAY_AMT6 = st.text_input("Amount paid in Last 6th month ( in (NT) $ )")
+    PAY_AMT1 = st.text_input("Amount paid in Last Month ( in NT $)")
+    PAY_AMT2 = st.text_input("Amount paid in Last 2nd month ( in NT $ )")
+    PAY_AMT3 = st.text_input("Amount paid in Last 3rd month ( in NT $ )")
+    PAY_AMT4 = st.text_input("Amount paid in Last 4th month ( in NT $ )")
+    PAY_AMT5 = st.text_input("Amount paid in Last 5th month ( in NT $ )")
+    PAY_AMT6 = st.text_input("Amount paid in Last 6th month ( in NT $ )")
 
     if st.button("Predict"):
         
@@ -92,7 +92,7 @@ def main():
             st.success("This account will not be default with a probability of {}%.".format(round(np.max(probability)*100, 2)))
 
     if st.button("About"):
-        st.text("Predicts the defaulter and prevent losses to the lender.")
+        st.text("Predicts the defaulter and prevent losses to the financial firm.")
         st.text("Built by Raushan")
         
 if __name__ == '__main__':
